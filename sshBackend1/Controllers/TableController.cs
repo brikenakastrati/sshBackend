3c7abe3aa94ac3e5e0b3808d7c55e9bfe81b3d94
deallapatinca <dea.llapatinca@student.uni-pr.edu>
2025-04-14 10:44:51 +0200

    Fully functional APIs for Table

---
+++ sshBackend1/Controllers/TableController.cs
@@ -0,0 +1,208 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using sshBackend1.Models;
+using sshBackend1.Models.DTOs;
+using sshBackend1.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace sshBackend1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableController : ControllerBase
+    {
+        protected APIResponse _response;
+        private readonly ITableRepository _dbTable;
+        private readonly IMapper _mapper;
+
+        public TableController(ITableRepository dbTable, IMapper mapper)
+        {
+            _dbTable = dbTable;
+            _mapper = mapper;
+            _response = new();
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetTables()
+        {
+            try
+            {
+                IEnumerable<Table> tableList = await _dbTable.GetAllAsync();
+                _response.Result = _mapper.Map<List<TableDTO>>(tableList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "GetTable")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetTable(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var tableEntity = await _dbTable.GetAsync(u => u.TableId == id);
+                if (tableEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                _response.Result = _mapper.Map<TableDTO>(tableEntity);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateTable([FromBody] TableDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    return BadRequest("Invalid Table data.");
+                }
+
+
+
+                Table tableEntity = _mapper.Map<Table>(createDTO);
+                await _dbTable.CreateAsync(tableEntity);
+
+                _response.Result = _mapper.Map<TableDTO>(tableEntity);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetTable", new { id = tableEntity.TableId }, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteTable")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> DeleteTable(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var tableEntity = await _dbTable.GetAsync(u => u.TableId == id);
+                if (tableEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                await _dbTable.DeleteTableAsync(tableEntity);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id:int}", Name = "UpdateTable")]
+        public async Task<ActionResult<APIResponse>> UpdateTable(int id, [FromBody] TableDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null || id != updateDTO.TableId)
+                {
+                    return BadRequest();
+                }
+
+                Table model = _mapper.Map<Table>(updateDTO);
+
+                await _dbTable.UpdateTableAsync(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string>() { ex.ToString() };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+
+        }
+
+        [HttpPatch("{id:int}", Name = "UpdatePartialTable")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdatePartialTable(int id, JsonPatchDocument<TableDTO> patchDTO)
+        {
+            if (patchDTO == null || id == 0)
+            {
+                return BadRequest();
+            }
+
+            var tableEntity = await _dbTable.GetAsync(u => u.TableId == id, tracked: false);
+
+            if (tableEntity == null)
+            {
+                return BadRequest();
+            }
+
+            TableDTO tableDTO = _mapper.Map<TableDTO>(tableEntity);
+            patchDTO.ApplyTo(tableDTO, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Table model = _mapper.Map<Table>(tableDTO);
+            await _dbTable.UpdateTableAsync(model);
+
+            return NoContent();
+        }
+
+
+    }
+}
