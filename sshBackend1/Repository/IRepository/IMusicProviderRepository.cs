8fbd05dbeaaea5c095335d5cf81a1385ed948647
deallapatinca <dea.llapatinca@student.uni-pr.edu>
2025-04-15 11:05:26 +0200

    Repository for music provider

---
+++ sshBackend1/Repository/IRepository/IMusicProviderRepository.cs
@@ -0,0 +1,17 @@
+﻿
+using Nest;
+using sshBackend1.Models;
+using System.Linq.Expressions;
+
+namespace sshBackend1.Repository.IRepository
+{
+    public interface IMusicProviderRepository : IRepository<MusicProvider>
+    {
+        Task<IEnumerable<MusicProvider>> GetAllMusicProvidersAsync(Expression<Func<MusicProvider, bool>> filter = null);
+        Task<MusicProvider> GetMusicProviderAsync(Expression<Func<MusicProvider, bool>> filter = null);
+        Task CreateMusicProviderAsync(MusicProvider entity);
+        Task<MusicProvider> UpdateMusicProviderAsync(MusicProvider entity);
+        Task DeleteMusicProviderAsync(MusicProvider entity);
+        Task SaveAsync();
+    }
+}
