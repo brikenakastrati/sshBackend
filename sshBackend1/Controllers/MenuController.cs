e6c542f501f6c167ac88e4ce873b78d0fb4f32c9
deallapatinca <dea.llapatinca@student.uni-pr.edu>
2025-04-15 14:26:55 +0200

    API controller for Menu

--- sshBackend1/Controllers/MenuController.cs
+++ sshBackend1/Controllers/MenuController.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using sshBackend1.Data;
 using sshBackend1.Models;
+using sshBackend1.Models.DTOs;
+using sshBackend1.Repository.IRepository;
 
 namespace sshBackend1.Controllers
 {
@@ -14,95 +19,196 @@ namespace sshBackend1.Controllers
     [ApiController]
     public class MenuController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        protected APIResponse _response;
+        private readonly IMenuRepository _dbMenu;
+        private readonly IMapper _mapper;
 
-        public MenuController(ApplicationDbContext context)
+        public MenuController(IMenuRepository dbMenu, IMapper mapper)
         {
-            _context = context;
+            _dbMenu = dbMenu;
+            _mapper = mapper;
+            _response = new();
         }
 
-        // GET: api/Menu
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Menu>>> GetMenu()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetMenus()
         {
-            return await _context.Menu.ToListAsync();
+            try
+            {
+                IEnumerable<Menu> menuList = await _dbMenu.GetAllAsync();
+                _response.Result = _mapper.Map<List<MenuDTO>>(menuList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
         }
 
-        // GET: api/Menu/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Menu>> GetMenu(int id)
+        [HttpGet("{id:int}", Name = "GetMenu")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetMenu(int id)
         {
-            var menu = await _context.Menu.FindAsync(id);
-
-            if (menu == null)
+            try
             {
-                return NotFound();
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var menuEntity = await _dbMenu.GetAsync(u => u.MenuId == id);
+                if (menuEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                _response.Result = _mapper.Map<MenuDTO>(menuEntity);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreateMenu([FromBody] MenuDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    return BadRequest("Invalid Menu data.");
+                }
+
+                if (await _dbMenu.GetAsync(u => u.MenuName.ToLower() == createDTO.MenuName.ToLower()) != null)
+                {
+                    ModelState.AddModelError("ErrorsMessages", "Menu already exists!");
+                    return BadRequest(ModelState);
+                }
+
+                Menu menuEntity = _mapper.Map<Menu>(createDTO);
+                await _dbMenu.CreateAsync(menuEntity);
+
+                _response.Result = _mapper.Map<MenuDTO>(menuEntity);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetMenu", new { id = menuEntity.MenuId }, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpDelete("{id:int}", Name = "DeleteMenu")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> DeleteMenu(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var menuEntity = await _dbMenu.GetAsync(u => u.MenuId == id);
+                if (menuEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                await _dbMenu.DeleteMenuAsync(menuEntity);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id:int}", Name = "UpdateMenu")]
+        public async Task<ActionResult<APIResponse>> UpdateMenu(int id, [FromBody] MenuDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null || id != updateDTO.MenuId)
+                {
+                    return BadRequest();
+                }
+
+                Menu model = _mapper.Map<Menu>(updateDTO);
+
+                await _dbMenu.UpdateMenuAsync(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string>() { ex.ToString() };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
             }
 
-            return menu;
         }
 
-        // PUT: api/Menu/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutMenu(int id, Menu menu)
+        [HttpPatch("{id:int}", Name = "UpdatePartialMenu")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdatePartialMenu(int id, JsonPatchDocument<MenuDTO> patchDTO)
         {
-            if (id != menu.MenuId)
+            if (patchDTO == null || id == 0)
             {
                 return BadRequest();
             }
 
-            _context.Entry(menu).State = EntityState.Modified;
+            var menuEntity = await _dbMenu.GetAsync(u => u.MenuId == id, tracked: false);
 
-            try
+            if (menuEntity == null)
             {
-                await _context.SaveChangesAsync();
+                return BadRequest();
             }
-            catch (DbUpdateConcurrencyException)
+
+            MenuDTO menuDTO = _mapper.Map<MenuDTO>(menuEntity);
+            patchDTO.ApplyTo(menuDTO, ModelState);
+
+            if (!ModelState.IsValid)
             {
-                if (!MenuExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return BadRequest(ModelState);
             }
+
+            Menu model = _mapper.Map<Menu>(menuDTO);
+            await _dbMenu.UpdateMenuAsync(model);
 
             return NoContent();
-        }
-
-        // POST: api/Menu
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Menu>> PostMenu(Menu menu)
-        {
-            _context.Menu.Add(menu);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetMenu", new { id = menu.MenuId }, menu);
-        }
-
-        // DELETE: api/Menu/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteMenu(int id)
-        {
-            var menu = await _context.Menu.FindAsync(id);
-            if (menu == null)
-            {
-                return NotFound();
-            }
-
-            _context.Menu.Remove(menu);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool MenuExists(int id)
-        {
-            return _context.Menu.Any(e => e.MenuId == id);
         }
     }
 }
