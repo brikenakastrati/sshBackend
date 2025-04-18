d880b3928f3303b87ad39d8a204d7d0f893ef475
deallapatinca <dea.llapatinca@student.uni-pr.edu>
2025-04-18 11:47:57 +0200

    Pastry API Controller

--- sshBackend1/Controllers/PastryController.cs
+++ sshBackend1/Controllers/PastryController.cs
@@ -1,12 +1,13 @@
+using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using sshBackend1.Models;
+using sshBackend1.Models.DTOs;
+using sshBackend1.Repository.IRepository;
 using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using sshBackend1.Data;
-using sshBackend1.Models;
 
 namespace sshBackend1.Controllers
 {
@@ -14,95 +15,195 @@ namespace sshBackend1.Controllers
     [ApiController]
     public class PastryController : ControllerBase
     {
-        private readonly ApplicationDbContext _context;
+        protected APIResponse _response;
+        private readonly IPastryRepository _dbPastry;
+        private readonly IMapper _mapper;
 
-        public PastryController(ApplicationDbContext context)
+        public PastryController(IPastryRepository dbPastry, IMapper mapper)
         {
-            _context = context;
+            _dbPastry = dbPastry;
+            _mapper = mapper;
+            _response = new();
         }
 
-        // GET: api/Pastry
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Pastry>>> GetPastries()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<APIResponse>> GetPastries()
         {
-            return await _context.Pastries.ToListAsync();
+            try
+            {
+                IEnumerable<Pastry> pastryList = await _dbPastry.GetAllAsync();
+                _response.Result = _mapper.Map<List<PastryDTO>>(pastryList);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
         }
 
-        // GET: api/Pastry/5
-        [HttpGet("{id}")]
-        public async Task<ActionResult<Pastry>> GetPastry(int id)
+        [HttpGet("{id:int}", Name = "GetPastry")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> GetPastry(int id)
         {
-            var pastry = await _context.Pastries.FindAsync(id);
-
-            if (pastry == null)
+            try
             {
-                return NotFound();
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var pastryEntity = await _dbPastry.GetAsync(u => u.PastryId == id);
+                if (pastryEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                _response.Result = _mapper.Map<PastryDTO>(pastryEntity);
+                _response.StatusCode = HttpStatusCode.OK;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<APIResponse>> CreatePastry([FromBody] PastryDTO createDTO)
+        {
+            try
+            {
+                if (createDTO == null)
+                {
+                    return BadRequest("Invalid pastry data.");
+                }
+
+                if (await _dbPastry.GetAsync(u => u.PastryName.ToLower() == createDTO.PastryName.ToLower()) != null)
+                {
+                    ModelState.AddModelError("ErrorsMessages", "Pastry already exists!");
+                    return BadRequest(ModelState);
+                }
+
+                Pastry pastryEntity = _mapper.Map<Pastry>(createDTO);
+                await _dbPastry.CreateAsync(pastryEntity);
+
+                _response.Result = _mapper.Map<PastryDTO>(pastryEntity);
+                _response.StatusCode = HttpStatusCode.Created;
+                return CreatedAtRoute("GetPastry", new { id = pastryEntity.PastryId }, _response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [HttpDelete("{id:int}", Name = "DeletePastry")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<APIResponse>> DeletePastry(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                {
+                    _response.StatusCode = HttpStatusCode.BadRequest;
+                    return BadRequest(_response);
+                }
+
+                var pastryEntity = await _dbPastry.GetAsync(u => u.PastryId == id);
+                if (pastryEntity == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    return NotFound(_response);
+                }
+
+                await _dbPastry.DeletePastryAsync(pastryEntity);
+
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string> { ex.Message };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPut("{id:int}", Name = "UpdatePastry")]
+        public async Task<ActionResult<APIResponse>> UpdatePastry(int id, [FromBody] PastryDTO updateDTO)
+        {
+            try
+            {
+                if (updateDTO == null || id != updateDTO.PastryId)
+                {
+                    return BadRequest();
+                }
+
+                Pastry model = _mapper.Map<Pastry>(updateDTO);
+
+                await _dbPastry.UpdatePastryAsync(model);
+                _response.StatusCode = HttpStatusCode.NoContent;
+                _response.IsSuccess = true;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorsMessages = new List<string>() { ex.ToString() };
+                return StatusCode(StatusCodes.Status500InternalServerError, _response);
             }
 
-            return pastry;
         }
 
-        // PUT: api/Pastry/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPut("{id}")]
-        public async Task<IActionResult> PutPastry(int id, Pastry pastry)
+        [HttpPatch("{id:int}", Name = "UpdatePartialPastry")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> UpdatePartialPastry(int id, JsonPatchDocument<PastryDTO> patchDTO)
         {
-            if (id != pastry.PastryId)
+            if (patchDTO == null || id == 0)
             {
                 return BadRequest();
             }
 
-            _context.Entry(pastry).State = EntityState.Modified;
+            var pastryEntity = await _dbPastry.GetAsync(u => u.PastryId == id, tracked: false);
 
-            try
+            if (pastryEntity == null)
             {
-                await _context.SaveChangesAsync();
+                return BadRequest();
             }
-            catch (DbUpdateConcurrencyException)
+
+            PastryDTO pastryDTO = _mapper.Map<PastryDTO>(pastryEntity);
+            patchDTO.ApplyTo(pastryDTO, ModelState);
+
+            if (!ModelState.IsValid)
             {
-                if (!PastryExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
+                return BadRequest(ModelState);
             }
+
+            Pastry model = _mapper.Map<Pastry>(pastryDTO);
+            await _dbPastry.UpdatePastryAsync(model);
 
             return NoContent();
-        }
-
-        // POST: api/Pastry
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Pastry>> PostPastry(Pastry pastry)
-        {
-            _context.Pastries.Add(pastry);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPastry", new { id = pastry.PastryId }, pastry);
-        }
-
-        // DELETE: api/Pastry/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePastry(int id)
-        {
-            var pastry = await _context.Pastries.FindAsync(id);
-            if (pastry == null)
-            {
-                return NotFound();
-            }
-
-            _context.Pastries.Remove(pastry);
-            await _context.SaveChangesAsync();
-
-            return NoContent();
-        }
-
-        private bool PastryExists(int id)
-        {
-            return _context.Pastries.Any(e => e.PastryId == id);
         }
     }
 }
